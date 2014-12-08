79cedc9f830a986079886c7554acbd470c222361
samy <kandasamy@cmu.edu>
2014-12-07 21:07:30 -0500

    updated UCB utility function

--- BOLibkky/getUCBUtility.m
+++ BOLibkky/getUCBUtility.m
@@ -7,7 +7,7 @@ function util = getUCBUtility(x, funcH, numEvals)
   delta = 0.01; % something we need to set.
   t = numEvals + 1;
 %   beta_t = 2 * log( numDims * (t*pi)^2 / (6 * delta) ) / 5;
-  beta_t = 2 * log( 1000 * (t*pi)^2 / (6*delta) ) /5;
+  beta_t = 2 *(numDims * log(1000) + log( (t*pi)^2 / (6*delta) )) /5;
 %   beta_t = 2 * 10 + 300 * log(t)^(numDims + 1) * (log(t/delta))^3;
 
   % Obtain mean and standard deviation
