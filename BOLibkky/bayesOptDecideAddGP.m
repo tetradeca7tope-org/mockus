967c460e3cad09d99ea9d54eadcd0e0c3376f99c
Fan Yang <iamyangfan930@gmail.com>
2015-04-01 01:18:46 -0400

    solved dimension dismatch issues

--- BOLibkky/bayesOptDecideAddGP.m
+++ BOLibkky/bayesOptDecideAddGP.m
@@ -12,28 +12,39 @@ DECOMP_DECIDE = 'decide';
 
 if ~strcmp(params.decompStrategy, DECOMP_DECIDE)
   [maxVal, maxPt, boQueries, boVals, history] = ...
-  bayesOptDecompAddGP(oracle, decomp, bounds, numIters, params)
+  bayesOptDecompAddGP(oracle, decomp, bounds, numIters, params);
 
 else
   boQueries = [];
   boVals = [];
+  history = [];
 
   numDecompChoices = numel(decomp);
-  
+  numInnerIters = floor(numIters / numDecompChoices);
+
   for i=1:numDecompChoices
     thisDecomp.d = decomp{i}.d;
     thisDecomp.M = decomp{i}.M;
     params.decompStrategy = DECOMP_PLEARN;
 
     [maxVal, maxPt, thisBoQueries, thisBoVals, thisHistory]=...
-    bayesOptDecompAddGP(oracle, thisDecomp, bounds, numIters, params);
-    
-    boQueries = [boQueries;thisBoQueries];
-    boVals = [boVals;thisBoVals];
-    
+    bayesOptDecompAddGP(oracle, thisDecomp, bounds, numInnerIters, params);
+   
+    size(thisBoQueries);
+    size(thisBoVals);
+    if (i==1)
+      boQueries = [boQueries;thisBoQueries];
+      boVals = [boVals;thisBoVals];
+    else 
+      histLen = numel(thisBoVals);
+      boQueries = [boQueries;thisBoQueries(histLen-numInnerIters+1:end,:)];
+      boVals = [boVals;thisBoVals(histLen-numInnerIters+1:end)];
+    end
+
     params.initPts = boQueries;
     params.initVals = boVals; 
-
   end
   
+  size(boVals)
+end
 end
