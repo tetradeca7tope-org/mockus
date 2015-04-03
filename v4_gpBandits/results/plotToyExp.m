d75c662419a8e2983cbf3992ff7a46389fbf4091
Fan Yang <iamyangfan930@gmail.com>
2015-04-03 00:30:59 -0400

    clean up

--- v4_gpBandits/results/plotToyExp.m
+++ v4_gpBandits/results/plotToyExp.m
@@ -2,28 +2,27 @@
 
 % compare know decomposition, unknown grouping, and d varies across iterarions
 
-totalPts = size(boKDCumRegrets,2);
+mks = {'+','o','*','x','s','d','^','v','>','<','p','h'};
 
-figure(1)
-% simple regrets
+totalPts = size(boKDCumRegrets,2);
 labels = cell(2+numel(numDimsPerGroupCands),1);
 
-semilogy((1:totalPts),boKDSimpleRegrets(2,:),'color','green','Marker','+');
+cls = varycolor(2+numel(numDimsPerGroupCands));
+
+semilogy((1:totalPts),boKDSimpleRegrets(1,:),'color',cls(1,:),'Marker',mks{1});
 labels{1} = sprintf('Known Decomposition: true d = %d', trueNumDimsPerGroup);
 hold on
 
 for i=1:numel(numDimsPerGroupCands)
-  i=2;
   tmp = numel(numDimsPerGroupCands);
-  semilogy((1:totalPts),boAddSimpleRegrets(2,:,i),'color',[1 i/tmp 1-i/tmp], 'linewidth',3);
+  semilogy((1:totalPts),boAddSimpleRegrets(1,:,i),'color',cls(i+1,:),'Marker',mks{i+1});
   labels{i+1} = sprintf('Additive model, d = %d',numDimsPerGroupCands(i));
   hold on
 end
 
-semilogy((1:totalPts),boUDSimpleRegrets(2,:),'color','blue','Marker','o');
-
+semilogy((1:totalPts),boUDSimpleRegrets(1,:),'color',cls(i+2,:),'Marker',mks{i+2});
 
 labels{i+2} = sprintf('Decomposition varies through iterations');
 
-%legend(labels, 'FontSize',15,'Location','northeast');
+legend(labels, 'FontSize',15,'Location','northeast');
 title('Simple Regrets','FontSize',18);
