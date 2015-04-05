a475bb1bc355a0e32c947d287095be5f9a51a965
Fan Yang <iamyangfan930@gmail.com>
2015-04-05 00:47:05 -0400

    modified testing script

--- v4_gpBandits/v4_decide.m
+++ v4_gpBandits/v4_decide.m
@@ -13,30 +13,29 @@ addpath ../utils/
 warning off;
 
 % Problem parameters
-% IMPORTANT ASSUMPTION: size(numDimsPerGroupCands)-1 divides numIters
 
 % non-trivial experiment
-numExperiments = 2;
-%numDims = 40;
-%numDiRectEvals = 500;
-%trueNumDimsPerGroup = 8;
-%numIters = 300;
-%numDimsPerGroupCands = [40 4 8 10 16 18]';
+numExperiments = 1;
+numDims = 40;
+numDiRectEvals = 200;
+trueNumDimsPerGroup = 12;
+numIters = 200;
+numDimsPerGroupCands = [8 2 5 10 15 20 40]';
 
 % tiny experiments 
-numExperiments = 1;
-numDims = 30;
-numDiRectEvals = 100;
-trueNumDimsPerGroup = 12;
-numIters = 60;
-numDimsPerGroupCands = [30 2 4 8 12 15]';
+%numExperiments = 1;
+%numDims = 30;
+%numDiRectEvals = 10;
+%trueNumDimsPerGroup = 3;
+%numIters = 10;
+%numDimsPerGroupCands = [30 2 4]';
 
 numdCands = numel(numDimsPerGroupCands);
 
 % Initialize (d,M) pairs that passed to bayesOptDecideAddGP
-decomp = cell(numdCands-1,1);
-for i = 1:numdCands-1
-  decomp{i}.d = numDimsPerGroupCands(i+1);
+decomp = cell(numdCands,1);
+for i = 1:numdCands
+  decomp{i}.d = numDimsPerGroupCands(i);
   decomp{i}.M = floor(numDims / decomp{i}.d);
 end
 
@@ -115,25 +114,25 @@ boUDCumRegrets = zeros(numExperiments, totalNumQueries);
 
 for expIter = 1:numExperiments
 
-  fprintf('\n==============================================================\n');
-  fprintf('Experiment %d/ %d\nMaxVal: %0.4f\n', ...
-    expIter, numExperiments, trueMaxVal);
-  fprintf('Num DiRectEvals: %d\n', numDiRectEvals);
-  fprintf('==============================================================\n');
-
-  % Known true decomposition
-  fprintf('\nKnown Decomposition\n');
-  boKDParams.noises = 0 * ones(trueNumGroups, 1);
-  [~, ~, ~, valHistKD] = ...
-    bayesOptDecompAddGP(func, trueDecomp, bounds, numIters, boKDParams);
-  [sR, cR] = getRegrets(trueMaxVal, valHistKD);
-
-  boKDHistories(expIter, :) = valHistKD';
-  boKDSimpleRegrets(expIter, :) = sR';
-  boKDCumRegrets(expIter, :) = cR';
-
-  % Learn Decomposition
-  fprintf('\nKnown Grouping Unknown Decomposition\n');
+%  fprintf('\n==============================================================\n');
+%  fprintf('Experiment %d/ %d\nMaxVal: %0.4f\n', ...
+%    expIter, numExperiments, trueMaxVal);
+%  fprintf('Num DiRectEvals: %d\n', numDiRectEvals);
+%  fprintf('==============================================================\n');
+%
+%  % Known true decomposition
+%  fprintf('\nKnown Decomposition\n');
+%  boKDParams.noises = 0 * ones(trueNumGroups, 1);
+%  [~, ~, ~, valHistKD] = ...
+%    bayesOptDecompAddGP(func, trueDecomp, bounds, numIters, boKDParams);
+%  [sR, cR] = getRegrets(trueMaxVal, valHistKD);
+%
+%  boKDHistories(expIter, :) = valHistKD';
+%  boKDSimpleRegrets(expIter, :) = sR';
+%  boKDCumRegrets(expIter, :) = cR';
+%
+%  % Learn Decomposition
+%  fprintf('\nKnown Grouping Unknown Decomposition\n');
 
   % For the candidates in numDimsPerGroupCands
   for candIter = 1:numdCands
@@ -149,7 +148,7 @@ for expIter = 1:numExperiments
     [~, ~, ~, valHistAdd] = ...
       bayesOptDecompAddGP(func, decompAdd, bounds, numIters, boAddParamsCurr);
     
-    fprintf('In additive model, size of valHistAdd %d\n', numel(valHistAdd));
+    
 
     [sR, cR] = getRegrets(trueMaxVal, valHistAdd);
     boAddHistories(expIter, :, candIter) = valHistAdd';
@@ -162,24 +161,22 @@ for expIter = 1:numExperiments
   % Decomposition varies across iterations
   fprintf('\nChoose the decomposition at each iteration\n');
 
-  [~, ~, ~, valHistDecide, chosen] = ...
-      bayesOptDecideAddGP(func, decomp, bounds, numIters, boUDParams, numDims); 
+  [~, ~, ~, valHistDecide,~, dMHist] = ...
+      decide(func, decomp, bounds, numIters, boUDParams); 
   
   [sR, cR] = getRegrets(trueMaxVal, valHistDecide);
   
-  size(valHistDecide);
-  size(boUDHistories);
   boUDHistories(expIter, :) = valHistDecide';
   boUDSimpleRegrets(expIter, :) = sR';
   boUDCumRegrets(expIter, :) = cR';
 
-  % Save Results at each iteration
+ % Save Results at each iteration
   save(saveFileName, 'numDims', 'trueNumDimsPerGroup', 'func', ...
     'funcProperties', 'trueMaxVal',  ...
     'numIters', 'totalNumQueries', ...
     'boKDHistories', 'boAddHistories', 'boUDHistories', ...
     'boKDSimpleRegrets', 'boAddSimpleRegrets', 'boUDSimpleRegrets', ...
     'boKDCumRegrets', 'boAddCumRegrets', 'boUDCumRegrets', ...
-    'numDimsPerGroupCands', 'chosen');
+    'numDimsPerGroupCands', 'dMHist');
 
 end
