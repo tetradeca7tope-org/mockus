92136f609a084dd8ed7546ab2589c1db5e103288
Fan Yang <fanyang1@andrew.cmu.edu>
2015-05-20 14:13:21 -0400

    add ;, so no print

--- addGPLibkky/combinedKernelNoise.m
+++ addGPLibkky/combinedKernelNoise.m
@@ -2,7 +2,7 @@ function K = ...
     combinedKernelNoise(X, decomposition, bws, scales, noises, commonNoise)
   % Same as complete kernel but with noise
   K = combinedKernel(X, X, decomposition, bws, scales) + ...
-    (sum(noises) + commonNoise) * eye(size(X, 1))
+    (sum(noises) + commonNoise) * eye(size(X, 1));
   % scale the kernel
   numGroups = numel(decomposition);
   K = K / numGroups;
