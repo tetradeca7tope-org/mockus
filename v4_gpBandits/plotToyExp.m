34592b998dd23d26f7e751384657ef08d3008596
Fan Yang <fanyang1@andrew.cmu.edu>
2015-05-21 18:26:41 -0400

    plot a subset of all iteration plots

--- v4_gpBandits/plotToyExp.m
+++ v4_gpBandits/plotToyExp.m
@@ -1,29 +1,34 @@
 % assume the data is already loaded
-
-mks = {'+','^','v','<','>','p','h','.'};
+mks = {'^','v','<','>','p','h','.'};
 
 numExps = size(boUDCumRegrets,1)
 totalPts = size(boUDCumRegrets,2);
+NUM_SHOW = totalPts / 4;
+allIters = (1:totalPts);
 
 labels = cell(2+numel(numDimsPerGroupCands),1);
-cls = varycolor(2+numel(numDimsPerGroupCands));
+cls = varycolor(numel(numDimsPerGroupCands));
+
+qqq = round(linspace(1,totalNumQueries, NUM_SHOW));
 
 boAddSimpleRegrets = mean(boAddSimpleRegrets,1);
-
 for i=1:numel(numDimsPerGroupCands)
-  semilogy((1:totalPts),boAddSimpleRegrets(1,:,i),'color',cls(i,:),'Marker',mks{i});
+  semilogy(allIters(qqq), boAddSimpleRegrets(1,qqq,i),'color',cls(i,:),'Marker',mks{i});
   labels{i} = sprintf('Add-d = %d',numDimsPerGroupCands(i));
   hold on
 end
 
 boUDSimpleRegrets = mean(boUDSimpleRegrets,1);
-semilogy((1:totalPts),boUDSimpleRegrets(1,:),'color',cls(1+i,:),'Marker',mks{1+i});
-labels{1+i} = sprintf('Decomposition varies');
+semilogy(allIters(qqq),boUDSimpleRegrets(1,qqq),'color','black','Marker','o');
+labels{1+i} = sprintf('CHOOSEdM');
 hold on
 
 randSimpleRegrets = mean(randSimpleRegrets,1);
-semilogy((1:totalPts),randSimpleRegrets(1,:),'color',cls(2+i,:),'Marker',mks{2+i});
-labels{2+i} = sprintf('Random optimization');
+semilogy(allIters(qqq),randSimpleRegrets(1,qqq),'color','red','Marker','+');
+labels{2+i} = sprintf('Random');
 
-legend(labels, 'FontSize',15,'Location','northeast');
-title('Simple Regrets','FontSize',18);
+legend(labels, 'FontSize',12,'Location','northeast');
+ylabel('Simple Regrets','FontSize',12);
+xlabel('Number of iterations','FontSize',12);
+titleStr = sprintf('(D, d, M) = (%d, %d, %d)', numDims, trueNumDimsPerGroup, floor(numDims/trueNumDimsPerGroup));
+title(titleStr,'FontSize',12);
