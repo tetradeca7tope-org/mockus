358436e2b6ea57e9809be3b6cb89379224feb249
Fan Yang <iamyangfan930@gmail.com>
2015-05-22 20:14:29 -0400

    plot multiple choosedm lines

--- v4_gpBandits/plotToyExp.m
+++ v4_gpBandits/plotToyExp.m
@@ -1,31 +1,38 @@
 % assume the data is already loaded
-mks = {'^','v','<','>','p','h','.'};
-
 numExps = size(boUDCumRegrets,1)
 totalPts = size(boUDCumRegrets,2);
 NUM_SHOW = totalPts / 4;
 allIters = (1:totalPts);
 
-labels = cell(2+numel(numDimsPerGroupCands),1);
-cls = varycolor(numel(numDimsPerGroupCands));
+notRandLines = numel(numDimsPerGroupCands) + numel(choosedM);
 
+cls = varycolor(notRandLines);
+clsAdd = cls(1:numel(numDimsPerGroupCands),:);
+clsUD = cls(end-numel(choosedM)+1:end,:);
+
+mksAdd = {'^','v','<','>','.'};
+mksUD = {'o','p','h'};
+
+labels = cell(notRandLines+1,1);
 qqq = round(linspace(1,totalNumQueries, NUM_SHOW));
 
 boAddSimpleRegrets = mean(boAddSimpleRegrets,1);
 for i=1:numel(numDimsPerGroupCands)
-  semilogy(allIters(qqq), boAddSimpleRegrets(1,qqq,i),'color',cls(i,:),'Marker',mks{i});
+  semilogy(allIters(qqq), boAddSimpleRegrets(1,qqq,i),'color',clsAdd(i,:),'Marker',mksAdd{i});
   labels{i} = sprintf('Add-d = %d',numDimsPerGroupCands(i));
   hold on
 end
 
 boUDSimpleRegrets = mean(boUDSimpleRegrets,1);
-semilogy(allIters(qqq),boUDSimpleRegrets(1,qqq),'color','black','Marker','o');
-labels{1+i} = sprintf('CHOOSEdM');
-hold on
+for i=1:numel(choosedM)
+  semilogy(allIters(qqq),boUDSimpleRegrets(1,qqq,i),'color', clsUD(i,:),'Marker', mksUD{i});
+  labels{numel(numDimsPerGroupCands)+i} = choosedM{i};
+  hold on
+end
 
 randSimpleRegrets = mean(randSimpleRegrets,1);
 semilogy(allIters(qqq),randSimpleRegrets(1,qqq),'color','red','Marker','+');
-labels{2+i} = sprintf('Random');
+labels{end} = sprintf('Random');
 
 legend(labels, 'FontSize',12,'Location','northeast');
 ylabel('Simple Regrets','FontSize',12);
